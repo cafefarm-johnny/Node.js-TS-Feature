cdf09ac0dcce0bc05da784f06a59217a566c7ce7
Johnny <npcdja@worldspon.com>
2020-01-29 18:04:07 +0900

    passport 설정 주석 추가
    passport.config.ts
    - serializeUser > 주석을 추가했다
    - deserializeUser > 주석을 추가했다

--- config/passport.config.ts
+++ config/passport.config.ts
@@ -9,6 +9,8 @@ export default (passport: PassportStatic) => {
      * @param user.id 사용자 고유번호
      */
     passport.serializeUser((user: LoginSession, done) => {
+        // authenticate에서 인증에 성공하면 req.login 속성에서 id값을 빼온다.
+        // id를 제외한 나머지 필드 값은 쿠키로 만들어버린다.
         done(null, user.id);
     });
 
@@ -18,7 +20,9 @@ export default (passport: PassportStatic) => {
      * @param id 사용자 고유번호
      */
     passport.deserializeUser(async (id: number, done) => {
+        // 클라이언트 요청이 발생할 때 마다 deserial 한다.
         try {
+            // db를 조회해서 존재하는지 판단 후
             const member = await Member.findByPk(id, {
                 attributes: ['id', 'username', 'email']
             });
@@ -27,7 +31,7 @@ export default (passport: PassportStatic) => {
                 return done(null, false);
             }
 
-            return done(null, { ...member.get() });
+            return done(null, { ...member.get() }); // req.user에 저장한다.
         } catch (e) {
             console.error('----- Passport deserializeUser -----');
             return done(e);
