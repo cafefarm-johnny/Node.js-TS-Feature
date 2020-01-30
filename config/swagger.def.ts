b30d223286512c8e1b2edc1dd8369ae000ba6607
Johnny <npcdja@worldspon.com>
2020-01-30 17:54:18 +0900

    Swagger API 문서 샘플 작성

--- config/swagger.def.ts
+++ config/swagger.def.ts
@@ -14,11 +14,33 @@ const swaggerDefinition: SwaggerDefinition = {
     basePath: '/',
     contact: { email: 'npcdja@gmail.com' },
     // 모든 API에 대한 공통 정의
-    components: { res: {} },
+    components: {
+        res: {
+            Forbidden: { description: '권한이 없습니다.' },
+            NotFound: { description: '존재하지 않는 정보입니다.' }
+        }
+    },
     // 사용 가능한 통신 방식 (http, https, ws)
     schemes: ['http'],
     // DB 모델 정의
-    definitions: {}
+    definitions: {
+        Member: {
+            type: 'object',
+            properties: {
+                id: { type: 'number', description: '사용자 고유번호' },
+                username: { type: 'string', description: '사용자 아이디' },
+                password: { type: 'string', description: '사용자 비밀번호' },
+                beforePassword: {
+                    type: 'string',
+                    description: '사용자 이전 비밀번호'
+                },
+                email: { type: 'string', description: '사용자 이메일' },
+                createAt: { type: 'string', description: '생성일' },
+                updateAt: { type: 'string', description: '수정일' },
+                deleteAt: { type: 'string', description: '삭제일' }
+            }
+        }
+    }
 };
 
 export default {
--- src/member/route/login.route.ts
+++ src/member/route/login.route.ts
@@ -6,18 +6,57 @@ export default (app: Application) => {
     /**
      * 회원 테이블 재생성 테스트 라우트
      * @author Johnny
+     * @swagger
+     * /test:
+     *  get:
+     *      summary: 회원 테이블 재생성 테스트
+     *      responses:
+     *          200:
+     *              description: 재생성 성공
+     *              schema:
+     *                  type: object
+     *                  properties:
+     *                      message:
+     *                          type: string
+     *                          example: ok
      */
     app.get('/test', LoginController.reCreateTable);
 
     /**
      * RSA 공개 키 요청 라우트
      * @author Johnny
+     * @swagger
+     * /getRSAPublicKey:
+     *  get:
+     *      summary: RSA 공개 키 요청
+     *      responses:
+     *          200:
+     *              description: 요청 성공
+     *              schema:
+     *                  type: object
+     *                  properties:
+     *                      publicKey:
+     *                          type: string
+     *                          example: MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCty6kY07bbg8xbFRJk/Xc6kAInhj8vd/VnG5MHbBbWCikm+h7z4yGmwthUkWyKVYtm8M2xojnfzw9Id90qjsFSdKEmK0j3aLJBzQJDbWB+967/1Tndc4NGFUn1ScXRC9r4QWdO4ZqezQdy7NOtbCTYGVbwEDzrmtAd0/eiZHFn7wIDAQAB
      */
     app.get('/getRSAPublicKey', LoginController.getRSAPublicKey);
 
     /**
      * 비밀번호 암호화 요청 라우트
      * @author Johnny
+     * @swagger
+     * /passwordHash:
+     *  get:
+     *      summary: 비밀번호 암호화 요청
+     *      responses:
+     *          200:
+     *              description: 요청 성공
+     *              schema:
+     *                  type: object
+     *                  properties:
+     *                      hashed:
+     *                          type: string
+     *                          exmaple: 5EG9Al5YnZsqoQcxAkr30v0A6WdN2MM2Lz802NODJwo=
      */
     app.get('/passwordHash', LoginController.getEncryptPassword);
 };
